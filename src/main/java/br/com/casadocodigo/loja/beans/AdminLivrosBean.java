4eebb93416cefd5baa5079bdd24146f6f34ddf4e
Albertino Neto <albeneto72@gmail.com>
2020-04-18 22:37:19 -0300

    Criação do projeto casadocodigo com JEE/CDI/JPA/HIBERNATE/JSF

---
+++ src/main/java/br/com/casadocodigo/loja/beans/AdminLivrosBean.java
@@ -0,0 +1,33 @@
+package br.com.casadocodigo.loja.beans;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.transaction.Transactional;
+
+import br.com.casadocodigo.loja.dao.LivroDao;
+import br.com.casadocodigo.loja.models.Livro;
+
+@Named
+@RequestScoped
+public class AdminLivrosBean {
+
+	@Inject
+	private LivroDao livroDao;
+	
+	private Livro livro = new Livro();
+	
+	public Livro getLivro() {
+		return livro;
+	}
+	
+	public void setLivro(Livro livro) {
+		this.livro = livro;
+	}
+	
+	@Transactional
+	public void salvar() {
+		System.out.println("Livro cadastrado " + livro);
+		livroDao.salvar(livro);
+	}
+}
